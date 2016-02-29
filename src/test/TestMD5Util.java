4d024c57b70d3c463033849cc71a87b51ed0b365
lp5 <liupeng3425@qq.com>
2016-02-29 20:48:04 +0800

    20160229 night

---
+++ src/test/TestMD5Util.java
@@ -0,0 +1,15 @@
+package test;
+
+import utils.MD5Util;
+
+/**
+ * Created by Peng on 2016/2/29.
+ * test class MD5Util.
+ */
+public class TestMD5Util {
+    public static void main(String[] args) {
+        String plainCode = "liupeng";
+        System.out.println("origin code: " + plainCode);
+        System.out.println("MD5: " + MD5Util.getMD5Code(plainCode));
+    }
+}
